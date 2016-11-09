57ecd1688fd61b3f54810fdcc7d4a2b36e0f370f
andrews kuhn da silva <andrewskuhn@hotmail.com>
2016-11-09 13:46:57 -0200

    Computa sinais.

--- fpu.c
+++ fpu.c
@@ -4,72 +4,129 @@
 #define BIT_FANTASMA 8388608
 
 typedef union {
-	float f;
-	struct {
-		unsigned int mantissa : 23;
-		unsigned int expoente : 8;
-		unsigned int sinal : 1;
-	} field;
+    float f;
+    struct {
+        unsigned int mantissa : 23;
+        unsigned int expoente : 8;
+        unsigned int sinal : 1;
+    } field;
 } unionfloat;
 
-unionfloat somaValores(unionfloat float1, unionfloat float2) {
+int getBit(unsigned int number, const short int position) {
+    if (number & (1 << (position - 1)))
+        return 1;
+    else
+        return 0;
+}
+
+int computaSinal(int houveTroca, int houveComplemento, unsigned int sinal1, unsigned int sinal2) {
+    //se sinais forem iguais
+    if(sinal1 == sinal2) return sinal1;
     
-    unionfloat resultado = float1;
-    unsigned int resultadoMantissa;
-    unsigned int mantissa1 = float1.field.mantissa + BIT_FANTASMA;
-    unsigned int mantissa2 = float2.field.mantissa + BIT_FANTASMA;
-    char vaiUm, i, diferencaExpoente = float1.field.expoente - float2.field.expoente;
+    const struct {
+        unsigned int houveTroca : 1;
+        unsigned int houveComplemento : 1;
+        unsigned int sinal1 : 1;
+        unsigned int sinal2 : 1;
+    } f = {houveTroca, houveComplemento, sinal1, sinal2};
+
+    if (
+            (f.houveTroca && f.sinal1 && !f.sinal2) ||
+            (!f.houveTroca && !f.houveComplemento && !f.sinal1 && f.sinal2) ||
+            (!f.houveTroca && f.houveComplemento && f.sinal1 && !f.sinal2)
+       )
+        return 0;
     
-    // Substitui valor2 pelo seu complemento de dois caso os sinais sejam diferentes
-    if(float1.field.sinal != float2.field.sinal)
-         mantissa2 = ~(mantissa2) + 1;
+    else 
+        if (    
+                (f.houveTroca && !f.sinal1 && f.sinal2) ||
+                (!f.houveTroca && !f.houveComplemento && f.sinal1 && !f.sinal2) ||
+                (!f.houveTroca && f.houveComplemento && !f.sinal1 && f.sinal2)
+            )
+        return 1;
+
     
-    // Coloca os dois valores sobre o mesmo expoente
-    if(diferencaExpoente > 0)
-        mantissa2 = mantissa2 >> diferencaExpoente;
-    
-    // Soma mantissa
-    resultadoMantissa = mantissa1 + mantissa2;
-    
-    // Verifica overflow
-    vaiUm = (resultadoMantissa >> 24) & 1;
-    
-    // Ajusta em caso do resultado negativo
-    if(float1.field.sinal != float2.field.sinal && diferencaExpoente == 0 && resultadoMantissa & BIT_FANTASMA && vaiUm == 1){
-        resultadoMantissa = ~(resultadoMantissa) +1;
-        vaiUm = (resultadoMantissa >> 24) & 1;
-    }
-    
-    // Normaliza
-    if(vaiUm == 1){
+
+}
+
+unionfloat normaliza(unionfloat structNumero, unsigned int mastissa, int short vaiUm) {
+    unionfloat resultado = structNumero;
+    unsigned int novaMatissa = mastissa;
+    //Se tiver vaiUm faz shift para direita e soma expoente
+    if (vaiUm) {
         resultado.field.expoente++;
-        resultadoMantissa = resultadoMantissa >> 1;
+        novaMatissa = novaMatissa >> 1;
+    } else {
+        //Se não shifta para esquerda até 1.0121 ....
+        while (!(novaMatissa & BIT_FANTASMA) && resultado.field.expoente) {
+            novaMatissa = novaMatissa << 1;
+            resultado.field.expoente--;
+        }
     }
-    else while(!(resultadoMantissa & BIT_FANTASMA) && resultado.field.expoente){
-        resultadoMantissa = resultadoMantissa << 1;
-        resultado.field.expoente--;
-    }
-    
-    resultado.field.mantissa = resultadoMantissa;
-    
+    resultado.field.mantissa = novaMatissa;
     return resultado;
 }
 
-main() {
-    
+unionfloat somaValores(unionfloat float1, unionfloat float2, const int troca) {
+
+    const int SINAIS_DIFERENTES = float1.field.sinal != float2.field.sinal;
+    int complemento = 0;
+    unionfloat resultado = float1;
+    unsigned int resultadoMantissa,
+            mantissa1 = float1.field.mantissa + BIT_FANTASMA,
+            mantissa2 = float2.field.mantissa + BIT_FANTASMA;
+    int short vaiUm,
+            diferencaExpoente = float1.field.expoente - float2.field.expoente;
+
+
+    // Substitui valor2 pelo seu complemento de dois caso os sinais sejam diferentes
+    if (SINAIS_DIFERENTES) {
+        mantissa2 = ~(mantissa2) + 1;       
+    }
+
+    // Coloca os dois valores sobre o mesmo expoente
+    if (diferencaExpoente > 0)
+        mantissa2 = mantissa2 >> diferencaExpoente;
+
+    // Soma mantissa
+    resultadoMantissa = mantissa1 + mantissa2;
+
+    // Verifica overflow
+    vaiUm = getBit(resultadoMantissa, 25);
+
+    // Ajusta em caso do resultado negativo
+    if (SINAIS_DIFERENTES && diferencaExpoente == 0 && (resultadoMantissa & BIT_FANTASMA) && vaiUm) {
+        resultadoMantissa = ~(resultadoMantissa) + 1;
+        vaiUm = getBit(resultadoMantissa, 25);
+        complemento = 1;
+    }
+
+    //Normaliza
+    resultado = normaliza(resultado, resultadoMantissa, vaiUm);
+    resultado.field.sinal = computaSinal(troca, complemento, float1.field.sinal, float2.field.sinal);
+
+    return resultado;
+
+}
+
+int main() {
+
     unionfloat float1, float2, resultado;
-    
-    printf("Primeiro valor: ");
-    scanf("%f", &float1.f);
-    printf("Segundo valor: ");
-    scanf("%f", &float2.f);
-    
+
+    //    printf("Primeiro valor: ");
+    //    scanf("%f", &float1.f);
+    //    printf("Segundo valor: ");
+    //    scanf("%f", &float2.f);
+
+    float1.f = 3;
+    float2.f = 3.1;
+
     // Verifica qual possui maior expoente e inverte caso necessario
-    if(float1.field.expoente >= float2.field.expoente)
-        resultado = somaValores(float1, float2);
+    if (float1.field.expoente >= float2.field.expoente)
+        resultado = somaValores(float1, float2, 0);
     else
-        resultado = somaValores(float2, float1);
+        resultado = somaValores(float2, float1, 1);
     
-    printf("Numero reconstituido: %f\n", pow(-1,(resultado.field.sinal)) * ( 1.0 + resultado.field.mantissa / pow(2,23)) * pow(2,(resultado.field.expoente - 127)));
-    
+    printf("Numero reconstituido: %f\n", pow(-1, (resultado.field.sinal)) * (1.0 + resultado.field.mantissa / pow(2, 23)) * pow(2, (resultado.field.expoente - 127)));
+    return 0;
 }
